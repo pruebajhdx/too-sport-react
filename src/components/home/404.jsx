2e704397a4413ad138931eb5f88d882e53fe5603
pruebajhdx <pruebajhdx@gmail.com>
2022-04-24 19:08:29 -0500

    new search

---
+++ src/components/home/404.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SomethingWrong = () => {
+  return (
+    <div class="not-found-page-content margin-top padding-top">
+      <h1 class="">404</h1>
+      <h2>Look like you're lost in space</h2>
+      <p class="text-color-gray margin-left margin-right">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </p>
+      <Link
+        to="/"
+        className="link back col display-inline-block button button-fill color-yellow text-color-black padding-left padding-right external"
+      >
+        Back to home
+      </Link>
+    </div>
+  );
+};
+
+export default SomethingWrong;
---
+++ src/components/routes/search/Search.jsx
@@ -0,0 +1,176 @@
+import Navbar from "../../home/Navbar";
+import $$ from "dom7";
+import { useState } from "react";
+import axios from "axios";
+
+const TitleInput = () => {
+  return (
+    <div className="newsman-block-title display-flex align-items-center justify-content-space-between">
+      <div>
+        <div className="block-title-medium no-margin block-title text-semibold">
+          Busqueda
+        </div>
+        <div className="newsman-opacity block-title no-margin newsman-block-subtitle">
+          Filtro de eventos disponibles del día
+        </div>
+      </div>
+      <div className="newsman-badge">
+        <a href="!" className="badge bg-color-white text-color-black">
+          ATENCIÓN
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const TagInput = () => {
+  return (
+    <div className="tags display-flex">
+      <div className="newsman-badge">
+        <a href="!" className="badge bg-color-white text-color-black">
+          Partido de futbol
+        </a>
+      </div>
+      <div className="newsman-badge">
+        <a href="!" className="badge bg-color-white text-color-black">
+          Ligas de futbol
+        </a>
+      </div>
+      <div className="newsman-badge">
+        <a href="!" className="badge bg-color-white text-color-black">
+          Canales
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const InputSearch = () => {
+  const background_color = "#36585a";
+  $$(".newsman-page-gradient").css("background-color", background_color);
+
+  const [value, setValue] = useState("");
+  const [football, setListFootball] = useState("");
+
+  const submitValue = {
+    value: value,
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      axios
+        .get(
+          `http://www.too-sport.com/api/search/football/${submitValue.value}`
+        )
+        .then((res) => {
+          setListFootball(res.data);
+        });
+    }
+  };
+  return (
+    <div className="newsman-block">
+      <TitleInput />
+      <TagInput />
+      <div className="newsman-block-content">
+        <div className="newsman-search">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-search"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+          </svg>
+          <input
+            type="text"
+            placeholder="Nueva busqueda"
+            className="input"
+            onChange={(e) => setValue(e.target.value)}
+            onKeyDown={handleKeyPress}
+          />
+        </div>
+        <ResultSearch data={football} />
+      </div>
+    </div>
+  );
+};
+
+const TitleResult = () => {
+  return (
+    <div className="newsman-block-title display-flex align-items-center justify-content-space-between">
+      <div>
+        <div className="block-title-medium no-margin block-title text-semibold">
+          Resultado
+        </div>
+        <div className="newsman-opacity block-title no-margin newsman-block-subtitle">
+          Filtro de busqueda
+        </div>
+      </div>
+      <div className="newsman-badge">
+        <a href="!" className="badge bg-color-white text-color-black">
+          RESPUESTA
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const ResultSearch = (props) => {
+  if (props.data.reponse !== undefined) {
+    if(props.data.reponse === "Search not found"){
+      return(<div> <p>No se encontro nada</p></div>)   
+    }
+    return (
+      <div className="newsman-block">
+        <TitleResult />
+        <div className="newsman-block-content">
+          <div className="blog-list-wrapper">
+            {props.data.reponse.map((value, idx) => (
+              <div key={idx} className="blog-list display-flex align-items-start">
+                <div className="blog-list-img newsman-object-fit">
+                  <img src={value.imgTitle} alt="bl1" />
+                </div>
+                <div className="blog-list-infos margin-left">
+                  <div className="newsman-badge">
+                    <a href="!" className="badge color-purple text-color-white">
+                      {'Servidor ' + value.nameLinks}
+                    </a>
+                  </div>
+                  <h2 className="margin-bottom-half">
+                    <a href="/single/">{value.name}</a>
+                  </h2>
+                  <span>{value.eventTime + ","} </span>
+                  <span>{ new Date().toDateString()+ "."}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div> 
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>Nada</p>
+      </div>
+    );
+  }
+};
+
+const Search = () => {
+  return (
+    <div>
+      <Navbar />
+      <InputSearch />
+    </div>
+  );
+};
+
+export default Search;
